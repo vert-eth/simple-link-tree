276fa9fcd04ab77df3214e072e9bca2cb726b5ac
vert.eth <101998556+vert-eth@users.noreply.github.com>
2022-04-07 04:30:30 +0000

    Late night commit - some updates, and a temporary hacky solution to the missing icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, require, keys, context } from "react";
 import './App.css';
 import jsonList from './links';
+import tw from './img/twitter.png';
+import git from './img/github.png';
+import logo from './img/vert-logo-new.png';
+
+var images = [tw, git];
 
 function App() {
-
-  // const linkList = useState([]);
-
+  
   let linkList = [];
   
   for (var l in jsonList) {
     let link = jsonList[l];
-    console.log("LINK ELEMENT: " + link.link);
-    console.log(link.icon + "   " + link.type)
     let newLink = {
       url: link.link,
       type: link.type,
-      icon: link.icon
+      path: link.icon,
+      name: link.name
     };
-    console.log(newLink);
     linkList.push(newLink);
   }
-
-  console.log(linkList[0].link);
   
   return (
     <main>
       <div className="dataContainer">
         <div className="innerContainer">
-          <div>
+          <div className="contentContainer">
+            <img className="bigLogo" src={logo} />
             <h2>vert.eth</h2>
-            <p></p>
+            <p>A paragraph will go here</p>
           </div>
           <div className="linkList">
             {linkList.map((a, i) => {
               return (
-                <div className="linkContainer" key={i + "container"}>
-                  <img className="linkIcons" src={a.icon} />
-                  <a className={"majorLink " + a.type} href={a.url} key={i + "_link"}  target="_blank" rel="noopener noreferrer">{a.url}</a>
-                </div>
+                <a className={"majorLink " + a.type} href={a.url} key={i + "_link"}  target="_blank" rel="noopener noreferrer">
+                  <div className="linkContainer" key={i + "container"}>
+                    <img className="linkIcons" src={images[i]} />
+                    {a.name}
+                  </div>
+                </a>
               )
             })}
+            
           </div>
         </div>
       </div>
